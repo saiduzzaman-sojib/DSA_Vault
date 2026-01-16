7be5c50ff82dbc3d1e82ffaaaf3053c3d0a49d53
Md Saiduzzaman <saiduzzaman.015.sojib@gmail.com>
2026-01-17 00:31:32 +0600

    Create Min_cost_path_1.cpp

---
+++ 07_DynamicProgramming/Min_cost_path_1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int findMinCost(vector<vector<int>>& cost, int x, int y) {
+    int m = cost.size();
+    int n = cost[0].size();
+
+    if (x >= m || y >= n) {
+        return INT_MAX;
+    }
+
+    if (x == m - 1 && y == n - 1) {
+        return cost[x][y];
+    }
+
+    int right = findMinCost(cost, x, y + 1);
+    int down = findMinCost(cost, x + 1, y);
+    int diagonal = findMinCost(cost, x + 1, y + 1);
+
+    int minPath = min({right, down, diagonal});
+
+    if (minPath == INT_MAX) return INT_MAX;
+
+    return cost[x][y] + minPath;
+}
+
+int minCost(vector<vector<int>>& cost) {
+    return findMinCost(cost, 0, 0);
+}
+
+int main() {
+    vector<vector<int>> cost = {
+        {1, 2, 3},
+        {4, 8, 2},
+        {1, 5, 3}
+    };
+
+    cout << minCost(cost) << endl;
+
+    return 0;
+}
