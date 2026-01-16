a11b634763ca8f3a073c382a89bb31c98c70ea40
Md Saiduzzaman <saiduzzaman.015.sojib@gmail.com>
2026-01-16 23:57:00 +0600

    Create House_Robber_Optimized.cpp

---
+++ 07_DynamicProgramming/House_Robber_Optimized.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int rob(vector<int>& money) {
+    int n = money.size();
+
+    if (n == 0) return 0;
+    if (n == 1) return money[0];
+
+    int prev2 = 0;
+    int prev = money[0];
+
+    for (int i = 1; i < n; i++) {
+        int pick = money[i] + prev2;
+        int skip = prev;
+
+        int curr = max(pick, skip);
+
+        prev2 = prev;
+        prev = curr;
+    }
+
+    return prev;
+}
+
+int main() {
+    vector<int> houses = {6, 7, 1, 3, 8, 2, 4};
+    
+    cout << "Maximum Stolen Value: " << rob(houses) << endl;
+    
+    return 0;
+}
