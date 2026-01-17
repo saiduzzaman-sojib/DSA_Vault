134184d68f4440bf01e3fc2a9d7e1d7f43a82648
Md Saiduzzaman <saiduzzaman.015.sojib@gmail.com>
2026-01-17 23:11:26 +0600

    Update Subset_Sum_Recursive.cpp

--- 07_DynamicProgramming/Subset_Sum_Recursive.cpp
+++ 07_DynamicProgramming/Subset_Sum_Recursive.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-bool isSubsetSum(vector<int>& arr, int n, int sum) {
+bool SubsetSum(vector<int>& arr, int n, int sum) {
     if (sum == 0) return true;
     if (n == 0) return false;
 
     if (arr[n - 1] > sum) {
-        return isSubsetSum(arr, n - 1, sum);
+        return SubsetSum(arr, n - 1, sum);
     }
 
-    return isSubsetSum(arr, n - 1, sum) || 
-           isSubsetSum(arr, n - 1, sum - arr[n - 1]);
+    return SubsetSum(arr, n - 1, sum) || 
+           SubsetSum(arr, n - 1, sum - arr[n - 1]);
 }
 
 int main() {
     vector<int> arr = {3, 34, 4, 12, 5, 2};
     int sum = 9;
 
-    if (isSubsetSum(arr, arr.size(), sum)) {
+    if (SubsetSum(arr, arr.size(), sum)) {
         cout << "True" << endl;
     } else {
         cout << "False" << endl;
