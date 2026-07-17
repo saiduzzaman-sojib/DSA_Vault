efb1a3e684909583a516d926436673da70f495a3
Md Saiduzzaman <saiduzzaman.015.sojib@gmail.com>
2026-07-17 20:39:42 +0600

    Implement Tram problem solution in C++

--- Problem-Solving/Codeforces/800/116A_Tram.cpp
+++ Problem-Solving/Codeforces/800/116A_Tram.cpp
@@ -1,1 +1,32 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    int max=0;
+    int total=0;
+    int arr[1005][2];
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+ 
+    for (int i = 0; i < n; i++)
+    {
+        total = total - arr[i][0] + arr[i][1];
+        
+        if(max < total){
+            max = total;
+        }
+    }
+    
+    cout<<max;
+    
+    return 0;
+}
