7482caa2ba7b1c70c5749d959a48913cdbd824ea
SAIDUZZAMAN SOJIB <saiduzzaman01313@gmail.com>
2026-08-25 00:34:19 +0600

    Add basic prefix sum

---
+++ Algorithms/Prefix_Sum/basic_prefixSum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+const int N=1e5+10;
+int arr[N];
+int pf[N];
+int main()
+{
+   int n;
+   cin>>n;
+   for (int  i = 1; i <=n; i++)
+   {
+   cin>>arr[i];
+   pf[i]=pf[i-1]+arr[i];
+   }
+   int l,r;
+   cin>>l>>r;
+   cout<<pf[r]-pf[l-1]<<endl;
+   
+    return 0;
+}
