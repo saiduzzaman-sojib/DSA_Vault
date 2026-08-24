cf2112444594ff4d644882f0e0a9681c8073ccdc
SAIDUZZAMAN SOJIB <saiduzzaman01313@gmail.com>
2026-08-24 23:03:53 +0600

    Just check the vs code terminal

--- Data-Structures/Arrays/deletion.c
+++ Data-Structures/Arrays/deletion.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
 
-int main() { 
-    int size,position,n;
-    printf("Enter the array size:");
-    scanf("%d",&size);
+int main() {
+    int size, n, position;
+
+    printf("Enter array size: ");
+    scanf("%d", &size);
+
     int arr[size];
-    printf("Enter the number of element:");
-    scanf("%d",&n);
-    printf("Enter the value of the array :\n");
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d",&arr[i]);
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    printf("Youe current array is :\n");
-    for (int  i = 0; i < n; i++)
-    {
-       printf("%d\n",arr[i]);
+
+    printf("Current array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
-    printf("Enter the position you want to delet value :\n");
-    scanf("%d",&position);
-    
-    for (int i = position-1; i < n-1; i++)
-    {
-        arr[i]=arr[i+1];
+
+    printf("\nEnter position to delete: ");
+    scanf("%d", &position);
+
+    for (int i = position - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
     }
-    printf("New array is :\n");
-    for (int i = 0; i < n-1; i++)
-    {
-        printf("%d\n",arr[i]);
+
+    n--;
+
+    printf("New array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
-    
+
     return 0;
 }
