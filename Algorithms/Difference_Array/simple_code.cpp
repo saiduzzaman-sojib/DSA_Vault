f721add36f023ab700f3ba929e685cd117a65452
saiduzzaman-sojib <saiduzzaman.015.sojib@gmail.com>
2026-08-28 23:24:58 +0600

    feat: add 1D difference array implementation with trace comments

--- Algorithms/Difference_Array/simple_code.cpp
+++ Algorithms/Difference_Array/simple_code.cpp
@@ -7,22 +7,21 @@ long long diff[N];
 long long a[N];
 
 void update(int l, int r, long long val) {
-    diff[l] += val;
-    diff[r + 1] -= val;
+    diff[l] += val;      
+    diff[r + 1] -= val;  
 }
 
 int main() {
     
     int n = 7;
 
-    update(0, 2, 5);//[5, 5, 5, 0, 0, 0, 0]
-    update(1, 3, 6);//[5, 11, 11, 6, 0, 0, 0]
-    update(2, 6, 1);//[5, 11, 12, 7, 1, 1, 1]
+    update(0, 2, 5); //[5, 0, 0, -5, 0, 0, 0]
+    update(1, 3, 6); //[5, 6, 0, -5, -6, 0, 0]
+    update(2, 6, 1); //[5, 6, 1, -5, -6, 0, 0]
 
     a[0] = diff[0];
     for (int i = 1; i < n; i++) {
-        diff[i] += diff[i - 1];
-        a[i] = diff[i];
+        a[i] = a[i - 1] + diff[i]; //[5,11,12,7,1,1,1]
     }
 
     for (int i = 0; i < n; i++) {
