4300f90dcd4b1cfc089fe2cf2c6c9dcc10c5f4cc
saiduzzaman-sojib <saiduzzaman.015.sojib@gmail.com>
2026-08-31 01:46:00 +0600

    Div-3 easy question

---
+++ Problem-Solving/Codeforces/800/The_67th_integer_problem.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    while (n--)
+    {
+
+        int t;
+        cin>>t;
+        if (t<67)
+        {
+             cout<<t+1<<endl;
+        }else cout<<67<<endl;
+
+    }
+    
+    return 0;
+}
