9f2ad2aa21742a18108c922fc7becc8f0e496178
saiduzzaman-sojib <saiduzzaman.015.sojib@gmail.com>
2026-08-31 23:49:39 +0600

    Add solution for div 3 contest problem Blackslex and password

---
+++ Problem-Solving/Codeforces/800/Blackslex_Password.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    while (n--)
+    {
+        int k,x;
+        cin>>k>>x;
+        cout<<(k*x)+1<<endl;
+    }
+    
+    return 0;
+}
