a5d4e8014b0a7704ca7f0ea006de6cad451bb09f
saiduzzaman-sojib <saiduzzaman.015.sojib@gmail.com>
2026-09-06 23:12:06 +0600

    Add Solution Codeforce The 67th Permutation Problem

---
+++ Problem-Solving/Codeforces/800/The_67th_Permutation_Problem.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++) {
+        cout << i << " " << n + 2 * i - 1 << " " << n + 2 * i << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
